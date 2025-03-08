5c712ea2460d8886037248edf5146f7f107f9353
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-03-08 14:12:36 -0500

    Add editor settings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,5 +2,5 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Search filenames in the worksplace' })
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Search files by text in the workspace' })
-vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Display neovim buffers' })
-
+vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Search neovim buffers' })
+vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Search git files' })
--- lua/ashrahma/set.lua
+++ lua/ashrahma/set.lua
@@ -12,3 +12,23 @@ vim.opt.softtabstop = curr_indent
 vim.opt.shiftwidth = curr_indent
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.undodir = os.getenv("HOME") ..  ".vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
