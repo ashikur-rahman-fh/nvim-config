36a518529a7a7e5332e5e6e807913e44e0524d3e
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-03-10 21:35:00 -0400

    Add missing common config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -94,6 +94,21 @@ cmp.setup.cmdline(':', {
 --- Language specific on attach of lspconfig
 ----------------------------------------------------
 
+local common_on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Keybindings
+  local opts = { buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+end
+
 -- Pyright (Python)
 local pyright_on_attach = function(client, bufnr)
   common_on_attach(client, bufnr) -- Include common functionality
